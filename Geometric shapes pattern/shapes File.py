704bac90d010a04fb1e0ed1feb3e3350b4ba1f31
Danish <idanishfaraaz@gmail.com>
2026-06-15 13:45:31 +0530

    Add files via upload

---
+++ Geometric shapes pattern/shapes File.py
@@ -0,0 +1,196 @@
+circle=[(5,1),(2,2),(1,5),(2,8),(5,9),(8,8),(9,5),(8,2)]
+def pat_circle():
+    print('CIRCLE')
+    for i in range(1,10):
+        for j in range(1,10):
+            if (i,j) in circle:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+        
+pat_circle()
+
+Rhombus=[(1,3),(1,4),(1,5),(1,6),(1,7),(3,2),(3,6),(5,1),(5,2),(5,3),(5,4),(5,5)]
+def pattern_rhombus():
+    print('RHOMBUS')
+    for i in range(1,10):
+        for j in range(1,10):
+            if (i,j) in Rhombus:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_rhombus()
+
+Oval=[(3,4),(3,5),(3,6),(4,2),(4,8),(5,1),(5,9),(6,1),(6,9),(7,2),(7,8),(8,4),(8,5),(8,6)]
+def pattern_oval():
+    print('OVAL')
+    for i in range(1,10):
+        for j in range(1,10):
+            if (i,j)in Oval:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_oval()
+
+Triangle=[(2,5),(3,4),(3,6),(4,3),(4,7),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8)]
+def pattern_triangle():
+    print('TRIANGLE')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Triangle:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_triangle()
+
+                  
+Semi_circle=[(4,4),(4,5),(4,6),(4,7),(5,3),(5,8),(6,2),(6,9),(7,2),(7,9),(8,2),(8,3),(8,4),(8,5),(8,6),(8,7),(8,8),(8,9)]
+def pattern_semi_circle():
+    print('SEMI_CIRCLE')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Semi_circle:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_semi_circle()
+
+Square=[(2,2),(2,3),(2,4),(2,5),(2,6),(2,7),(2,8),(3,2),(3,8),(4,2),(4,8),(5,2),(5,8),(6,2),(6,8),(7,2),(7,8),(8,2),(8,3),(8,4),(8,5),(8,6),(8,7),(8,8)]
+def pattern_Square():
+    print('SQUARE')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Square:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_Square()
+
+Rectangle=[(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(2,2),(2,9),(3,2),(3,9),(4,2),(4,3),(4,4),(4,5),(4,6),(4,7),(4,8),(4,9)]
+def pattern_rectangle():
+    print('RECTANGLE')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Rectangle:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_rectangle()
+
+Hexagon=[(2,4),(2,5),(2,6),(3,3),(3,7),(4,2),(4,8),(5,1),(5,9),(6,2),(6,8),(7,3),(7,7),(8,4),(8,5),(8,6)]
+def pattern_hexagon():
+    print('HEXAGON')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Hexagon:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_hexagon()
+
+Trapezoid=[(2,3),(2,4),(2,5),(2,6),(2,7),(3,2),(3,8),(4,1),(4,9),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8),(5,9)]
+def pattern_trapezoid():
+    print('TRAPEZOID')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Trapezoid:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_trapezoid()
+
+Heptagon=[(1,5),(2,3),(2,7),(3,2),(3,8),(4,2),(4,8),(5,2),(5,8),(6,3),(6,7),(7,4),(7,5),(7,6)]
+def pattern_heptagon():
+    print('HEPTAGON')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Heptagon:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_heptagon()
+
+Octagon=[(2,4),(2,5),(2,6),(3,3),(3,7),(4,2),(4,8),(5,2),(5,8),(6,2),(6,8),(7,3),(7,7),(8,4),(8,5),(8,6)]
+def pattern_octagon():
+    print('OCTAGON')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Octagon:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_octagon()
+
+Heart=[(2,2),(2,3),(2,7),(2,8),(3,1),(3,4),(3,6),(3,9),(4,1),(4,5),(4,9),(5,2),(5,8),(6,3),(6,7),(7,4),(7,6),(8,5)]
+def pattern_heart():
+    print('HEART')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Heart:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_heart()
+
+Decagon=[(1,4),(1,5),(1,6),(2,3),(2,7),(3,2),(3,8),(4,2),(4,8),(5,2),(5,8),(6,2),(6,8),(7,3),(7,7),(8,4),(8,5),(8,6)]
+def pattern_decagon():
+    print('DECAGON')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Decagon:
+                print('+',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_decagon()
+
+Star=[(1,5),(3,2),(3,3),(3,4),(3,6),(3,7),(3,8),(4,3),(4,7),(5,4),(5,6),(6,3),(6,5),(6,7),(7,2),(7,8)]
+def pattern_star():
+    print('STAR')
+    for i in range(1,10):
+        for j in range(1,10):
+            if(i,j)in Star:
+                print('*',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+        
+pattern_star()
+
